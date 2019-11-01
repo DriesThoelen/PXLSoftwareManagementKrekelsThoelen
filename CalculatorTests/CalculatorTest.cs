70f51bc93de41db5703eb0ffb100d92f477eec71
Reinaut Krekels <reinaut.krekels@outlook.com>
2019-11-01 16:02:49 +0100

    Test operator precedence and operation builder

--- CalculatorTests/CalculatorTest.cs
+++ CalculatorTests/CalculatorTest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Calculator;
 using NUnit.Framework;
 using Calculator.Operators;
 
@@ -90,6 +92,91 @@ namespace CalculatorTests
             //Assert
             var expected = arg1 / arg2;
             Assert.That(result, Is.EqualTo(expected));
+        }
+
+
+        private static IEnumerable<object> Operators()
+        {
+            yield return new object[] {AddOperation.Builder(), AddOperation.Symbol};
+            yield return new object[] {SubtractOperation.Builder(), SubtractOperation.Symbol};
+            yield return new object[] {MultiplyOperation.Builder(), MultiplyOperation.Symbol};
+            yield return new object[] {DivideOperation.Builder(), DivideOperation.Symbol};
+        }
+
+        [TestCaseSource(nameof(Operators))]
+        public void ShouldBuildOperation(object duoOperatorArgument, char operatorSign)
+        {
+            // Arrange
+            var duoOperator = duoOperatorArgument as IDuoOperationBuilder<DuoOperation>;
+            Assert.NotNull(duoOperator);
+            FixedValueOperation operandLeft = 0.0;
+            FixedValueOperation operandRight = 1.0;
+
+            // Act
+            duoOperator.WithLeftOperand(operandLeft);
+            duoOperator.WithRightOperand(operandRight);
+            var operation = duoOperator.Build();
+            var render = operation.ToString();
+
+            // Assert
+            Assert.IsInstanceOf(duoOperator.GetType().GetGenericArguments()[0], operation);
+            Assert.AreEqual($"({operandLeft} {operatorSign} {operandRight})", render);
+        }
+
+        [TestCase('+', '+')]
+        [TestCase('+', '-')]
+        [TestCase('-', '+')]
+        [TestCase('-', '-')]
+        [TestCase('*', '+')]
+        [TestCase('*', '-')]
+        [TestCase('*', '*')]
+        [TestCase('*', '/')]
+        [TestCase('/', '+')]
+        [TestCase('/', '-')]
+        [TestCase('/', '*')]
+        [TestCase('/', '/')]
+        public void ShouldPrioritizeFirstOperator(char firstSymbol, char secondSymbol)
+        {
+            // Arrange
+            const double operand = 1.0;
+
+            // Act
+            var render = RenderOperationTree(operand, firstSymbol, secondSymbol);
+
+            // Assert
+            Assert.AreEqual($"(({operand} {firstSymbol} {operand}) {secondSymbol} {operand})", render);
+        }
+
+        [TestCase('+', '*')]
+        [TestCase('+', '/')]
+        [TestCase('-', '*')]
+        [TestCase('-', '/')]
+        public void ShouldPrioritizeSecondOperator(char firstSymbol, char secondSymbol)
+        {
+            // Arrange
+            const double operand = 1.0;
+
+            // Act
+            var render = RenderOperationTree(operand, firstSymbol, secondSymbol);
+
+            // Assert
+            Assert.AreEqual($"({operand} {firstSymbol} ({operand} {secondSymbol} {operand}))", render);
+        }
+
+        private string RenderOperationTree(double operand, char firstSymbol, char secondSymbol)
+        {
+            // Arrange part
+            var operator1 = DuoOperationBuilder<DuoOperation>.FromSymbol(firstSymbol);
+            var operator2 = DuoOperationBuilder<DuoOperation>.FromSymbol(secondSymbol);
+            var operationTree = new OperationTree();
+
+            // Act part
+            operationTree.PushValue(operand);
+            operationTree.PushOperator(operator1);
+            operationTree.PushValue(operand);
+            operationTree.PushOperator(operator2);
+            operationTree.PushValue(operand);
+            return operationTree.ToString();
         }
     }
 }
