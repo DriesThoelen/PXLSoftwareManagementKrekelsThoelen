25f3b961531116b235281fca1c8d80b8150b3e2c
Reinaut Krekels <reinaut.krekels@outlook.com>
2019-11-10 12:34:59 +0100

    Move IBinaryOperationBuilder to own file (refactoring)

---
+++ Calculator/Operators/IBinaryOperationBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator.Operators
+{
+    // Used because C# doesn't allow covariant generic types (out T) in classes
+    internal interface IBinaryOperationBuilder<out T> where T : BinaryOperation
+    {
+        IBinaryOperationBuilder<T> WithLeftOperand(IOperation operand);
+        IBinaryOperationBuilder<T> WithRightOperand(IOperation operand);
+        int Priority { get; }
+        T Build();
+    }
+}
