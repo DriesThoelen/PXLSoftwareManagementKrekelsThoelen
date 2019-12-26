2bd32acc1c1332e14bee73caa24eddbe1f06580b
Reinaut Krekels <reinaut.krekels@outlook.com>
2019-12-26 21:29:02 +0100

    Revert to outputtype WinExe, to remove the console window.

--- Calculator/Operators/IOperation.cs
+++ Calculator/Operators/IOperation.cs
@@ -11,11 +11,12 @@
         /// </summary>
         /// <returns>The resulting value</returns>
         /// <exception cref="System.InvalidOperationException">If this operation cannot be calculated</exception>
+        /// <exception cref="System.ArithmeticException">If calculating this operation leads to arithmetic, casting or conversion errors</exception>
         /// <remarks>The result may be influenced by overflow, underflow, or other effects.
         /// This method does not guarantee sensible results on <i>weird</i> inputs.</remarks>
         double Calculate();
 
-        /// <remarks>Defined to inform the compiler this never returns null, using null c# 8's null reference handling</remarks>
+        /// <remarks>Defined to inform the compiler this never returns null, using c# 8's null reference handling</remarks>
         string ToString();
     }
 }
